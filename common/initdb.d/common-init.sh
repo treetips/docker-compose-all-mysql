3920786dbb4ba7d71b6b8237a99c036baa118e6a
username <username@users.noreply.github.com>
2020-03-22 20:25:47 +0900

    fix: fix a bug that unknown variable error occurred at startup on MySQL8

--- common/initdb.d/common-init.sh
+++ common/initdb.d/common-init.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+. /app/share/functions.sh
+
+initMysqlInitdbdMyCnf
+
 echo "#######################################################"
 echo "# Execute init scripts"
 echo "#######################################################"
 ls -lR /app
+
+mysql $(getMysqlInitdbdCommand) < /app/share/test_country.sql
